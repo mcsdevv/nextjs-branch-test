0414f5c68b14f617a61b9b0ff5091697e7db3012
mcsdevv <mail@mcs.dev>
2021-05-17 12:54:33 +0100

    commit 3

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>New Branch - Commit #2</h1>
+        <h1 className={styles.title}>New Branch - Commit #3</h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
